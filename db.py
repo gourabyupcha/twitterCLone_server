68668a657aa1065185a9a536092b00937791a459
gourabyupcha <gourabyupcha@gmail.com>
2025-05-29 14:21:40 +0530

    updated mongo config

--- db.py
+++ db.py
@@ -1,12 +1,18 @@
 # db.py
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 from pymongo import MongoClient
-import os
+import os, certifi
 from dotenv import load_dotenv
 
 load_dotenv()  # Loads environment variables from .env file
 
 # Initialize MongoDB client
-client = MongoClient(os.getenv("MONGO_URI"))
+client = MongoClient(os.getenv("MONGO_URI"), 
+    tls=True,
+    tlsCAFile=certifi.where())
 db = client["twitter_clone_db"] 
 
 # Export collections
